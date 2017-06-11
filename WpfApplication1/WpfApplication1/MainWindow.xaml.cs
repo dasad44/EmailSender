eb297d7157e1302a79be6d2021f4b249b4eb2449
dasad44 <dasad33_79@o2.pl>
2017-06-11 13:28:13 +0200

    cos

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int a = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("cokls.txt")
+            
+                writer.Write("Zapisuje tekst");
+                writer.WriteLine("Zapisuje tekst i tworze znak nowej linii");
+               
+            
+        }
+
+       
+    }
+}
