b119c4fc79f26fac2df0dfb39dbb37475df73acc
dasad44 <dasad33_79@o2.pl>
2021-02-09 00:26:33 +0100

    init

--- Program.cs
+++ Program.cs
@@ -10,14 +10,18 @@ namespace ConsoleApp
         {
             string host = "smtp.gmail.com";
             int i = 0;
+            string login, password, destmail;
+            login = Console.ReadLine();
+            password = Console.ReadLine();
+            destmail = Console.ReadLine();
             while (i < 20)
             {
                 SmtpClient client = new SmtpClient(host, 587);
-                client.Credentials = new NetworkCredential("21edqcds@gmail.com", "P@ssw0rd_");
+                client.Credentials = new NetworkCredential(login, password);
                 client.EnableSsl = true;
-                MailAddress from = new MailAddress("21edqcds@gmail.com");
+                MailAddress from = new MailAddress(login);
 
-                MailAddress to = new MailAddress("rufffiankax@gmail.com");
+                MailAddress to = new MailAddress(destmail);
                 MailMessage message = new MailMessage(from, to);
                 message.Body = "Kghkjhhjdjd" + i;
                 message.Subject = "Nfsafam sfaIe ! ;*" + i;
